63fb77d89b80e6b880c826a187e5c6fe51d3eb93
natacha <nat_bartu@hotmail.com>
2022-09-15 18:23:01 +1000

    get the elements into the DOM

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const toggles = document.querySelectorAll('.toggle');
+const good = document.querySelector('#good');
+const cheap = document.querySelector('#cheap');
+const fast = document.querySelector('#fast');
+#good
